920a533520a0ce5c88f2375f027bfd844cc8204d
r4sznovo <74591072+r4sznovo@users.noreply.github.com>
2022-11-25 14:15:09 +0200

    Add files via upload

---
+++ kasutaja_kustutamine_kysimisega.ps1
@@ -0,0 +1,12 @@
+﻿$Eesnimi = Read-Host -Prompt "Palun sisestage enda eesnimi"
+$Perenimi = Read-Host -Prompt "Palun sisestage enda perenimi"
+
+$Kasutajanimi = $Eesnimi.ToLower() + "." + $Perenimi.ToLower()
+
+#kasutaja eemaldamine
+if (Get-LocalUser | Where-Object Name -eq $Kasutajanimi) { # otsitakse üles antud eesnime ja perenime järgi kasutajanimi.
+    Remove-LocalUser -Name $Kasutajanimi #kui see on olemas, kustutakse kasutaja.
+    Write-Output "Kasutaja on kustutatud."
+} else {
+    Write-Output "$Kasutajanimi nimelist kasutajat ei ole olemas." #vastasel juhul antakse veateade, et sellist kasutajat ei ole olemas.
+}
---
+++ kasutaja_loomine_kysimisega.ps1
@@ -0,0 +1,18 @@
+﻿$Eesnimi = Read-Host -Prompt "Palun sisestage enda eesnimi"
+$Perenimi = Read-Host -Prompt "Palun sisestage enda perenimi"
+
+$Kasutajanimi = $Eesnimi.ToLower() + "." + $Perenimi.ToLower()
+$Taisnimi = $Eesnimi + " " + $Perenimi
+$KontoKirjeldus = "Lokaalne kasutaja"
+$Parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+# sellega kontrollib, kas sellise eesnime ja perenimega kasutaja eksisteerib (Measure-Object vaatab, kas selline kasutaja juba eksisteerib või mitte.)
+$usercheck = Get-LocalUser | Where-Object Name -eq $Kasutajanimi | Measure-Object
+
+# kui sellist kasutajat ei ole ehk on tühi (0), loodakse kasutaja antud eesnime ja perenime järgi.
+if ($usercheck.Count -eq 0) {
+    New-LocalUser $Kasutajanimi -password $Parool -FullName $Taisnimi -Description $KontoKirjeldus
+    echo "Kasutaja loomine õnnestus!"
+} else {
+    echo "Kasutaja juba eksisteerib."
+}
